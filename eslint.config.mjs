522811494326fc7a4871bfd55e32e5de22c7cbc3
Yousef Dawood <yousefdawood31@gmail.com>
2026-01-15 21:41:36 +0200

    Add simple-import-sort plugin to organize project's imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,38 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
+
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "simple-import-sort/imports": [
+        "warn",
+        {
+          groups: [
+            // Packages. `react` and `next` related packages come first.
+            [
+              "server-only",
+              "client-only",
+              "^react",
+              "^next",
+              "^@?\\w",
+              "^(@|@company|@ui|components|utils|config|vendored-lib)(/.*|$)",
+              "^.+\\.s?css$",
+            ],
+          ],
+        },
+      ],
+      "simple-import-sort/exports": "warn",
+    },
+  },
+
   // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
