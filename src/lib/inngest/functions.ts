9cff2ee11dccd0d2d27c3c75076b731e75a62b55
Yousef Dawood <yousefdawood31@gmail.com>
2026-01-19 01:23:53 +0200

    extract URLs using firecrawl and scrape content before generating text to prevent LLM's cutoff issues

--- src/lib/inngest/functions.ts
+++ src/lib/inngest/functions.ts
@@ -1,17 +1,50 @@
 import { google } from "@ai-sdk/google";
 import { generateText } from "ai";
+import { firecrawl } from "@/lib/firecrawl";
 import { inngest } from "@/lib/inngest/client";
+
+const URL_REGEX =
+  /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w\-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/gm;
 
 export const googleAI = inngest.createFunction(
   { id: "google-ai" },
   { event: "google-ai/generate-text" },
-  async ({ event, step }) => {
+  async ({
+    event: {
+      data: { prompt },
+    },
+    step,
+  }) => {
+    const extractedUrls = await step.run("extract-urls", async () => {
+      const getAllExtractedUrls = prompt.match(URL_REGEX);
+      return getAllExtractedUrls;
+    });
+
+    const scrapeExtractedUrls = await step.run(
+      "scrape-extracted-urls",
+      async () => {
+        // prettier-ignore
+        if (!extractedUrls || !extractedUrls.length)
+          return "";
+
+        const batchScrape = await firecrawl.batchScrape(extractedUrls, {
+          options: { formats: ["markdown"] },
+        });
+        const scrapedContent =
+          batchScrape.data?.map(({ markdown }) => markdown) ?? [];
+        return scrapedContent.join("\n\n\n\n");
+      },
+    );
+
     await step.run("generate text with google gemini", async () => {
       const { text } = await generateText({
         model: google("gemini-flash-latest"),
-        prompt: event.data.prompt,
-      });
+        prompt: `
+        Context: ${scrapeExtractedUrls}
 
+        Question: ${prompt}
+        `,
+      });
       return { response: text };
     });
   },
