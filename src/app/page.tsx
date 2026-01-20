80068a409e98b0e5353f7f65b6ad7f83ff0dd7a0
Yousef Dawood <yousefdawood31@gmail.com>
2026-01-20 11:32:36 +0200

    build a component for creating/importing projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,27 @@
-"use client";
-
+import { FaGithub as GithubIcon } from "react-icons/fa";
+import { LucideSparkle } from "lucide-react";
+import ProjectCreate from "@/features/projects/components/ProjectCreate";
 import ProjectHeader from "@/features/projects/components/ProjectHeader";
 
 export default function Page() {
   return (
     <main className="min-h-svh bg-muted flex flex-col justify-center items-center font-poppins">
-      <ProjectHeader />
+      <section className="max-w-[750px] w-full flex flex-col gap-7">
+        <ProjectHeader />
+
+        <section className="flex gap-2.5 w-full ">
+          <ProjectCreate
+            icon={<LucideSparkle className="size-7" />}
+            command="⌘J"
+            content="New"
+          />
+          <ProjectCreate
+            icon={<GithubIcon className="size-7" />}
+            command="⌘I"
+            content="Import"
+          />
+        </section>
+      </section>
     </main>
   );
 }
--- src/features/projects/components/ProjectHeader.tsx
+++ src/features/projects/components/ProjectHeader.tsx
@@ -2,7 +2,7 @@ import CodexiaLogo from "@/components/CodexiaLogo";
 
 export default function ProjectHeader() {
   return (
-    <header className="flex gap-3.5 items-center justify-center">
+    <header className="flex gap-3.5 self-start items-center justify-center ">
       <CodexiaLogo />
       <h1 className="text-5xl font-semibold">Codexia</h1>
     </header>
