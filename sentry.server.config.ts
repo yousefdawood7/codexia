539d16b79b7fc23ae5801d64ac11d307f3175ac1
Yousef Dawood <yousefdawood31@gmail.com>
2026-01-21 13:42:21 +0200

    remove unnecessary comments from Sentry configuration files

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -1,7 +1,3 @@
-// This file configures the initialization of Sentry on the server.
-// The config you add here will be used whenever the server handles a request.
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/
-
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
@@ -14,13 +10,7 @@ Sentry.init({
     }),
   ],
 
-  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
   tracesSampleRate: 1,
-
-  // Enable logs to be sent to Sentry
   enableLogs: true,
-
-  // Enable sending user PII (Personally Identifiable Information)
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
   sendDefaultPii: true,
 });
