3030711252c8d76f9e1ca7a2457946858084c586
Yousef Dawood <yousefdawood31@gmail.com>
2026-01-23 09:12:34 +0200

    add utility functions for name generation and date formatting

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,25 @@
 import { type ClassValue, clsx } from "clsx";
+import { formatDistance } from "date-fns";
 import { twMerge } from "tailwind-merge";
+import {
+  adjectives,
+  animals,
+  type Config,
+  uniqueNamesGenerator,
+} from "unique-names-generator";
 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
-}
+const customConfig: Config = {
+  dictionaries: [adjectives, animals],
+  separator: "-",
+  length: 2,
+};
+
+// to get out of calling impure function's rule in eslint
+export const getCurrentDate = () => Date.now();
+
+export const cn = (...inputs: ClassValue[]) => twMerge(clsx(inputs));
+
+export const generateRandomNames = () => uniqueNamesGenerator(customConfig);
+
+export const getFormatedTime = (beginDate: number, endDate: number) =>
+  formatDistance(beginDate, endDate, { addSuffix: true });
